dcb454c12b8c44f40eddf7ead9d3e5b2a2a0b955
ed100076 <60796682+ed100076@users.noreply.github.com>
2021-04-11 23:11:00 +0200

    Déplacement gauche droite

--- src/logic/Entity.java
+++ src/logic/Entity.java
@@ -9,13 +9,8 @@ public abstract class Entity extends Thread {
     private Level level;
 
     // Constructeurs
-<<<<<<< Updated upstream
     public Entity(int posi, int posj, Cellule[][] cellules, Level level) {
-        pos = new Position(posi,posj);
-=======
-    public Entity(int posi, int posj, Cellule[][] cellules) {
         pos = new Position(posi, posj);
->>>>>>> Stashed changes
         this.cellules = cellules;
         this.level = level;
     }
@@ -34,13 +29,12 @@ public abstract class Entity extends Thread {
         return this.cellules;
     }
 
-<<<<<<< Updated upstream
-    public Level getLevel(){
+    public Level getLevel() {
         return this.level;
-=======
+    }
+
     public Position getPosition() {
-        return thi
->>>>>>> Stashed changes
+        return this.pos;
     }
 
     // Set Attributs
@@ -48,19 +42,18 @@ public abstract class Entity extends Thread {
         this.symbol = symbol;
     }
 
-    public void setLevel(Level level){
+    public void setLevel(Level level) {
         this.level = level;
     }
 
     // Méthode de déplacement
-    public void up(Cellule[][] cellules) {        
-        if(pos.getPosi() > 0){
-            if(cellules[pos.getPosi()-1][pos.getPosj()].getDecor() == getLevel().getLadder()){
+    public void up(Cellule[][] cellules) {
+        if (pos.getPosi() > 0) {
+            if (cellules[pos.getPosi() - 1][pos.getPosj()].getDecor() == getLevel().getLadder()) {
                 cellules[pos.getPosi()][pos.getPosj()].setEntity(null);
                 pos.setPosi(pos.getPosi() - 1);
                 cellules[pos.getPosi()][pos.getPosj()].setEntity(this);
-            }
-            else if(cellules[pos.getPosi()-1][pos.getPosj()].getDecor() == getLevel().getFloor() && cellules[pos.getPosi()][pos.getPosj()].getDecor() == getLevel().getLadder()){
+            } else if (cellules[pos.getPosi() - 1][pos.getPosj()].getDecor() == getLevel().getFloor() && cellules[pos.getPosi()][pos.getPosj()].getDecor() == getLevel().getLadder()) {
                 cellules[pos.getPosi()][pos.getPosj()].setEntity(null);
                 pos.setPosi(pos.getPosi() - 2);
                 cellules[pos.getPosi()][pos.getPosj()].setEntity(this);
@@ -69,35 +62,57 @@ public abstract class Entity extends Thread {
     }
 
     public void down(Cellule[][] cellules) {
-        if(pos.getPosi() < cellules.length-2){     
-            if(cellules[pos.getPosi()+1][pos.getPosj()].getDecor() == getLevel().getLadder()){
+        if (pos.getPosi() < cellules.length - 2) {
+            if (cellules[pos.getPosi() + 1][pos.getPosj()].getDecor() == getLevel().getLadder()) {
                 cellules[pos.getPosi()][pos.getPosj()].setEntity(null);
                 pos.setPosi(pos.getPosi() + 1);
                 cellules[pos.getPosi()][pos.getPosj()].setEntity(this);
-            }   
-            else if(cellules[pos.getPosi()+1][pos.getPosj()].getDecor() == getLevel().getFloor() && cellules[pos.getPosi()][pos.getPosj()].getDecor() == getLevel().getLadder() && pos.getPosi() < cellules.length-3){
+            } else if (cellules[pos.getPosi() + 1][pos.getPosj()].getDecor() == getLevel().getFloor() && cellules[pos.getPosi()][pos.getPosj()].getDecor() == getLevel().getLadder() && pos.getPosi() < cellules.length - 3) {
                 cellules[pos.getPosi()][pos.getPosj()].setEntity(null);
                 pos.setPosi(pos.getPosi() + 2);
                 cellules[pos.getPosi()][pos.getPosj()].setEntity(this);
-            }    
+            }
         }
     }
 
     public void right(Cellule[][] cellules) {
-        if(pos.getPosj() < cellules[0].length-2){
-            cellules[pos.getPosi()][pos.getPosj()].setEntity(null);
-            pos.setPosj(pos.getPosj() + 1);
-            cellules[pos.getPosi()][pos.getPosj()].setEntity(this);
+        // La case droite n'est pas un sol
+        if (cellules[pos.getPosi()][pos.getPosj() + 1].getDecor() == getLevel().getFloor()) {
+            return;
+        }
+        // Pas dépasser les bords
+        if (pos.getPosj() >= cellules[0].length - 2) {
+            return;
+        }
+        // La case droite ne possède pas un sol sous elle
+        if (cellules[pos.getPosi() + 1][pos.getPosj() + 1].getDecor() != getLevel().getFloor()
+            && cellules[pos.getPosi() + 1][pos.getPosj() + 1].getDecor() != getLevel().getLadder()) {
+            return;
         }
 
+        cellules[pos.getPosi()][pos.getPosj()].setEntity(null);
+        pos.setPosj(pos.getPosj() + 1);
+        cellules[pos.getPosi()][pos.getPosj()].setEntity(this);
     }
 
     public void left(Cellule[][] cellules) {
-        if(pos.getPosj() > 1){
-            cellules[pos.getPosi()][pos.getPosj()].setEntity(null);
-            pos.setPosj(pos.getPosj() - 1);
-            cellules[pos.getPosi()][pos.getPosj()].setEntity(this);
+        // La case gauche n'est pas un sol
+        if (cellules[pos.getPosi()][pos.getPosj() - 1].getDecor() == getLevel().getFloor()) {
+            return;
         }
+        // Pas dépasser les bords
+        if (pos.getPosj() <= 1) {
+            return;
+        }
+        // La case gauche ne possède pas un sol sous elle
+        if (cellules[pos.getPosi() + 1][pos.getPosj() - 1].getDecor() != getLevel().getFloor()
+            && cellules[pos.getPosi() + 1][pos.getPosj() - 1].getDecor() != getLevel().getLadder()) {
+            return;
+        }
+
+        cellules[pos.getPosi()][pos.getPosj()].setEntity(null);
+        pos.setPosj(pos.getPosj() - 1);
+        cellules[pos.getPosi()][pos.getPosj()].setEntity(this);
     }
 
     // Test collision
--- src/logic/Game.java
+++ src/logic/Game.java
@@ -10,32 +10,34 @@ import serveur.phases.Phase;
 import utils.RawConsoleInput;
 
 public class Game {
-    // // Phase courante du jeu
-    // private Phase phaseCourante;
-    Level level;
-    // /**
-    //  * Modifier la phase courante
-    //  *
-    //  * @param phase Phase
-    //  */
-    // public void setPhaseCourante(Phase phase) {
-    //     this.phaseCourante = phase;
-    // }
 
-    // /**
-    //  * Retourner la phase courante
-    //  *
-    //  * @return Phase courante
-    //  */
-    // public Phase getPhaseCourante() {
-    //     return this.phaseCourante;
-    // }
+    // Phase courante du jeu
+    private Phase phaseCourante;
+
+    Level level;
+
+    /**
+     * Modifier la phase courante
+     *
+     * @param phase Phase
+     */
+    public void setPhaseCourante(Phase phase) {
+        this.phaseCourante = phase;
+    }
+
+    /**
+     * Retourner la phase courante
+     *
+     * @return Phase courante
+     */
+    public Phase getPhaseCourante() {
+        return this.phaseCourante;
+    }
 
     public Game() {
         level = new Level(0);
-        int i=0;
-        while(i != 200)
-        {           
+        int i = 0;
+        while (i != 200) {
             level.print();
             level.getAssiettes().print();
             Scanner s = new Scanner(System.in);
